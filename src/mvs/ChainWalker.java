f89e469484399420b45df38f58daf62a41f82191
Paul Cantrell <cantrell@pobox.com>
2019-11-14 01:36:45 -0600

    Added javadoc, cleaned up

--- src/mvs/ChainWalker.java
+++ src/mvs/ChainWalker.java
@@ -3,6 +3,11 @@ package mvs;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A helper class for both building the Markov chain and generating text with it. Clients can either
+ * repeatedly choose random words or pass in new words, and ChainWalker keeps track of the recently
+ * added/generated words.
+ */
 class ChainWalker {
     private final MarkVShaney mvs;
     private final List<String> context = new ArrayList<>();
@@ -11,6 +16,9 @@ class ChainWalker {
         this.mvs = mvs;
     }
 
+    /**
+     * Updates the recent context with the new word, e.g. “one fish” → “fish two” → “two fish.”
+     */
     private void shiftIntoContext(String word) {
         if (context.size() >= mvs.getContextSize()) {
             context.remove(0);
@@ -18,11 +26,18 @@ class ChainWalker {
         context.add(word);
     }
 
+    /**
+     * Adds a new word choice for the current context to the Markov chain, and updates the context.
+     */
     void addNext(String word) {
         mvs.addChoice(context, word);
         shiftIntoContext(word);
     }
 
+    /**
+     * Chooses a word appropriate to the current context at random from the Markov chain, and
+     * updates the context.
+     */
     String chooseNext() {
         String result = mvs.chooseNextWord(context);
         shiftIntoContext(result);
--- src/mvs/WordChoice.java
+++ src/mvs/WordChoice.java
@@ -2,37 +2,33 @@ package mvs;
 
 import java.util.*;
 
+/**
+ * Holds a collection of words to choose from, and allows clients to select one at random.
+ */
 final class WordChoice {
     private static final Random rand = new Random();
 
     private final List<String> choices = new ArrayList<>();
 
+    /**
+     * Returns all the words available to choose from.
+     */
     public List<String> getChoices() {
         return Collections.unmodifiableList(choices);
     }
 
+    /**
+     * Chooses one of the available words at random.
+     */
     public String getRandomChoice() {
         return choices.get(rand.nextInt(choices.size()));
     }
 
-    public boolean isEmpty() {
-        return choices.isEmpty();
-    }
-
+    /**
+     * Adds a new possible choice.
+     */
     public void addChoice(String choice) {
         choices.add(choice);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        WordChoice that = (WordChoice) o;
-        return Objects.equals(choices, that.choices);
     }
 
     @Override
